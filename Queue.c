e440da8dbe70075d1bfc068868dfd2bdd75ac9a3
Ubuntu <ubuntu@ip-172-31-36-155.ap-northeast-2.compute.internal>
2023-03-31 16:15:57 +0000

    2023-04-01 이진 검색 트리 구현 전

--- Queue.c
+++ Queue.c
@@ -20,12 +20,24 @@ void init_que(Queue* que){
     que->front =-1;
 }
 // 선형 큐 상태 출력
-
+void print_que(Queue* que){
+    for (int i=0;i<MAX_QUEUE_SIZE;i++){
+        if(i<=que->front || i>que->rear){
+            printf("   |");
+        }
+        else{
+            printf("%d |",que->arr[i]);
+        }
+        
+    }
+    printf("\n");
+}
 // 선형 큐가 포화상태인가?
-int is_full (Queue* que){
+int is_full(Queue* que){
     if (que->rear == MAX_QUEUE_SIZE-1){
         return 1;
     }
+
     return 0;
 }
 // 선형 큐가 공백상태인가?
@@ -36,9 +48,23 @@ int is_none(Queue* que){
     return 0;
 }
 // 선형 큐에 데이터 삽입
-void enque()
-// 선형 큐에서 데이터 제거
+void enque(Queue* que, element val){
+    if (is_full(que)){
+        printf("큐가 포화상태 입니다.\n");
+        exit(1);
+    }
 
+    que->arr[++(que->rear)] = val;
+}
+// 선형 큐에서 데이터 제거
+void deque(Queue* que){
+    if(is_none(que)){
+        printf("제거할 데이터가 없습니다.\n");
+        exit(1);
+    }
+
+    que->arr[++(que->front)];
+}
 // 메인
 int main(){
     Queue a;
@@ -47,7 +73,17 @@ int main(){
 
     init_que(&p);
 
+    enque(&p,10); print_que(&p);
+    enque(&p,20); print_que(&p);
+    enque(&p,30); print_que(&p);
+    enque(&p,30); print_que(&p);
+    enque(&p,30); print_que(&p);
 
+    deque(&p); print_que(&p);
+    deque(&p); print_que(&p);
+    deque(&p); print_que(&p);
 
+    
+    
     return 1;
 }
---
+++ Tree.c
@@ -0,0 +1,9 @@
+//https://yjg-lab.tistory.com/139
+
+// 삽입 연산
+
+// 삭제 연산 
+
+// 탐색 연산
+
+// main
